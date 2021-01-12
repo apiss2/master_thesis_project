c3d3ec163785f0922238ad8201997f01bd1a5137
apiss2 <apiss2ex@gmail.com>
2021-01-12 22:55:29 +0900

    Specification change of settings

--- settings.py
+++ settings.py
@@ -3,9 +3,6 @@ import shutil
 import argparse
 import configparser
 
-import sys
-sys.path.append('../')
-
 from src.utils import utils
 from src.dataset import albmentation_augmentation as aug
 
@@ -26,84 +23,110 @@ task_name = config.get('task_name')
 save_dir = os.path.join('D:/result/', task_name, experiment_name)
 os.makedirs(save_dir, exist_ok=True)
 
-# save config
-shutil.copy(args.config, os.path.join(save_dir, os.path.basename(args.config)))
-
 ############ settings ############
 
 # util
+geometric = config.get('geometric')
+cnn_output_dim = utils.geometric_choice(geometric)
 model_save_path = os.path.join(save_dir, config.get('model_save_name'))
 
 # training
 monitor_metric = config.get('monitor_metric')
-epochs = int(config.get('epochs'))
-modelupdate_freq = int(config.get('modelupdate_freq'))
+epochs = config.getint('epochs')
 
-# dataset
+modelupdate_freq = config.getint('modelupdate_freq')
+discupdate_freq = config.getint('discupdate_freq')
+
+### image path ###
+# train
+train_image_path = config.get('train_image_path')
+train_label_path = config.get('train_label_path')
+
 image_A_train_path = config.get('image_A_train')
 label_A_train_path = config.get('label_A_train')
 image_B_train_path = config.get('image_B_train')
 label_B_train_path = config.get('label_B_train')
+
+# valid
+valid_image_path = config.get('valid_image_path')
+valid_label_path = config.get('valid_label_path')
 
 image_A_valid_path = config.get('image_A_valid')
 label_A_valid_path = config.get('label_A_valid')
 image_B_valid_path = config.get('image_B_valid')
 label_B_valid_path = config.get('label_B_valid')
 
+# test
+test_image_path = config.get('test_image_path')
+test_label_path = config.get('test_label_path')
+
 image_A_test_path = config.get('image_A_test')
 label_A_test_path = config.get('label_A_test')
 image_B_test_path = config.get('image_B_test')
 label_B_test_path = config.get('label_B_test')
 
+# dataset
+mean, std = utils.get_normParam(config.get('mean'), config.get('std'))
 mean_A, std_A = utils.get_normParam(config.get('MR_mean'), config.get('MR_std'))
 mean_B, std_B = utils.get_normParam(config.get('CT_mean'), config.get('CT_std'))
-class_num = int(config.get('class_num'))
+
+class_num = config.getint('class_num')
 label_type = config.get('label_type')
 color_palette = utils.get_colorPalette(config.get('color_palette'))
 
 aug_settings_path = config.get('augmentation_setting_json')
 image_size = aug.get_transformedImageSize(aug_settings_path)
 
-# save settings
-if config.get('color_palette') is not None:
-    shutil.copy(config.get('color_palette'), os.path.join(save_dir, os.path.basename(config.get('color_palette'))))
-shutil.copy(aug_settings_path, os.path.join(save_dir, os.path.basename(aug_settings_path)))
+random_t_tps = config.getfloat('random_t_tps') # geometric only
 
 # dataloader
-batch_size = int(config.get('batch_size'))
-num_workers = int(config.get('num_workers'))
+batch_size = config.getint('batch_size')
+num_workers = config.getint('num_workers')
 
 # model
 model = config.get('model')
 encoder = config.get('encoder')
-depth = int(config.get('depth'))
+in_channels = config.getint('in_channels')
+depth = config.getint('depth')
 weights = config.get('weights')
 activation = config.get('activation')
 
+freeze = config.getboolean('freeze_FeatureExtractionModel')
 pretrained_model_path = config.get('pretrained_model_path')
 padding_mode = config.get('padding_mode')
 
-discriminator_channels = list(map(lambda x: int(x), config.get('discriminator_channels').split('-')))
+if config.get('discriminator_channels') is not None:
+    discriminator_channels = list(map(lambda x: int(x), config.get('discriminator_channels').split('-')))
 
 # loss func
-loss_seg = config.get('loss_seg')
-loss_DA = config.get('loss_DA')
+loss = config.get('loss')
+loss_D = config.get('loss_D')
 class_weight = utils.get_classWeight(config.get('class_weight'))
-
-if config.get('class_weight') is not None:
-    shutil.copy(config.get('class_weight'), os.path.join(save_dir, os.path.basename(config.get('class_weight'))))
 
 # metrics
 metrics = config.get('metrics').split('-')
-metrics_D = config.get('metrics_D').split('-')
-metrics_test = config.get('metrics_test').split('-')
+metrics_D = config.get('metrics_D').split('-') if config.get('metrics_D') is not None else None
+metrics_seg = config.get('metrics_seg').split('-') if config.get('metrics_seg') is not None else None
+metrics_test = config.get('metrics_test').split('-') if config.get('metrics_test') is not None else None
 
 # optimizer
 optimizer = config.get('optimizer')
-lr = float(config.get('lr'))
+lr = config.getfloat('lr')
 
 # scheduler
-decay_schedule = list(map(lambda x: int(x), config.get('decay_schedule').split('-')))
-gamma = float(config.get('gamma'))
+scheduler_type = config.get('scheduler_type')
+if config.get('decay_schedule') is not None:
+    decay_schedule = list(map(lambda x: int(x), config.get('decay_schedule').split('-')))
+gamma = config.getfloat('gamma')
+warmupepochs = config.getint('warmupepochs')
+eta_min = config.getfloat('eta_min')
 
+### save settings ###
+shutil.copy(args.config, os.path.join(save_dir, os.path.basename(args.config)))
+if config.get('color_palette') is not None:
+    shutil.copy(config.get('color_palette'), os.path.join(save_dir, os.path.basename(config.get('color_palette'))))
+if config.get('class_weight') is not None:
+    shutil.copy(config.get('class_weight'), os.path.join(save_dir, os.path.basename(config.get('class_weight'))))
+if aug_settings_path is not None:
+    shutil.copy(aug_settings_path, os.path.join(save_dir, os.path.basename(aug_settings_path)))
 
